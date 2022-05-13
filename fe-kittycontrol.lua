45ff1cbf15b13a3e887c31d80a5f8acd9aafca90
martiin <60437627+martinelcrac@users.noreply.github.com>
2022-05-12 21:41:36 -0400

    Create fe-kittycontrol.lua

---
+++ fe-kittycontrol.lua
@@ -0,0 +1,10 @@
+--https://www.roblox.com/catalog/5313491692/Mimzy-The-Cat
+--fe kitty control
+getgenv().show = "e";
+
+--cap
+if syn then
+ setfpscap(60) --only u have fps unlocker
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/martinelcrac/fe-random/raw/rar-kittycontrol.lua"))();
